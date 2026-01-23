79e765aaa8d492c5b4b6228db2125a0b7d1698c0
sarahroon <sarahrooney130500@gmail.com>
2026-01-23 09:18:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let cookies = 0;
 
+const cookieCount = document.getElementById("cookieCount");
+const cookieButton = document.getElementById("cookieButton");
+
+cookieButton.addEventListener("click", () => {
+    cookies++;
+    cookieCount.textContent = cookies;
+});
